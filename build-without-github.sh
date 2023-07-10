06ef982693bdbe89f442d3fdd4fb630c59186435
Nate Carlson <git@natecarlson.com>
2023-07-10 10:08:14 -0500

    Updated to build docker images for both rm520 and rm50x.

--- build-without-github.sh
+++ build-without-github.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# Build newer version for RM520
 docker build . -f Dockerfile.rm520-modem-buildenv --tag natecarlson/rm520-build-env:1.0
+
+# Build for RM50x
+docker build . -f Dockerfile.rm50x-modem-buildenv --tag natecarlson/rm50x-build-env:1.0
